6cfe6f3b8af87a84e809dc5c9eebe74194099e9c
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-03-23 16:52:45 +0000

    Update
    adding the home page and setting the navigation

---
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarousel/MainCarosel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarosel/>
+    </div>
+  )
+}
+
+export default HomePage
