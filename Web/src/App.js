d884172bf5bfffa7388503ea29f550441bd2bb00
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-03-31 17:41:25 +0000

    Update Product.jsx
    Creating the product page that contains  the product cards and styyling them with the ProductCard.css

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navigation from './customer/components/navigation/Navigation.jsx';
+import Product from './customer/components/Product/Product.jsx';
 import Footer from './customer/Footer/Footer';
-import HomePage from './customer/Pages/HomePage/HomePage';
+// import HomePage from './customer/Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="">
       <Navigation/>
       <div>
-        <HomePage/>
+        {/* <HomePage/> */}
+        <Product/>
       </div>
       <Footer/>
     </div>
