a9597a33622dd7d6d83385637232944d3fd1a8d9
latifadidialaoui <didialaoui.latifa@gmail.com>
2024-04-01 13:55:34 +0000

    Update ProductDetails.jsx

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navigation from './customer/components/navigation/Navigation.jsx';
 import Product from './customer/components/Product/Product.jsx';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails.jsx';
 import Footer from './customer/Footer/Footer';
 // import HomePage from './customer/Pages/HomePage/HomePage';
 
@@ -10,7 +11,8 @@ function App() {
       <Navigation/>
       <div>
         {/* <HomePage/> */}
-        <Product/>
+         {/* <Product/> */}
+         <ProductDetails/>
       </div>
       <Footer/>
     </div>
