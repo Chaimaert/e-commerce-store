3851f3e86a010db4352fc7b7c42671966d3d35dd
chaimaert <chaima.Rouita@emsi-edu.ma>
2024-04-03 22:16:18 +0000

    Update App.css

--- Web/src/customer/components/ProductDetails/ProductDetails.jsx
+++ Web/src/customer/components/ProductDetails/ProductDetails.jsx
@@ -67,7 +67,8 @@ export default function ProductDetails() {
     const [selectedSize, setSelectedSize] = useState(product.sizes[2])
 
     return (
-        <div className="bg-white">
+        <div 
+        className="bd_color">
             <div className="pt-6">
                 <nav aria-label="Breadcrumb">
                     <ol role="list" className="mx-auto flex max-w-2xl items-center space-x-2 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
